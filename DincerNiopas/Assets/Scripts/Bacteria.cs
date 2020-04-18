bbafa746dc81f0a749f9700e92ecd513c0d5d5b3
barisdincer <barisdincer@sabanciuniv.edu>
2020-04-18 14:43:30 +0200

    Basic Bacteria Spawners, Bacterias and Cell has been added

---
+++ DincerNiopas/Assets/Scripts/Bacteria.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bacteria : MonoBehaviour
+{
+    [SerializeField] GameObject targetCell;
+    private int moveSpeed = 3;
+    private float rotateSpeed = 20f;
+    Rigidbody2D rb;
+    Vector2 targetPosition;
+    
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        targetPosition = new Vector2(targetCell.transform.position.x, targetCell.transform.position.y);
+    }
+
+    void FixedUpdate()
+    {
+        if (targetCell != null)
+        {
+            rb.velocity = transform.forward * moveSpeed;
+            Quaternion targetRotation = Quaternion.LookRotation(targetCell.transform.position - transform.position);
+            rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed));
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), targetPosition, moveSpeed * Time.fixedDeltaTime);
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ DincerNiopas/Assets/Scripts/BacteriaSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BacteriaSpawner : MonoBehaviour
+{
+	[SerializeField] Bacteria bacteriaPrefab;
+    // Start is called before the first frame update
+    float startAfterTime = 2.0f;
+    float repeatingTime = 2.0f;
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", startAfterTime, repeatingTime);
+    }
+
+    void Spawn () {
+        var bacteria = Instantiate(bacteriaPrefab, transform.position, Quaternion.identity);
+    }
+
+
+
+}
